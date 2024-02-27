88e168b65ffe58969eff6ff70b31429492486d7e
EWE Code <111604338+eatwanderexplore@users.noreply.github.com>
2024-02-27 13:08:55 +1300

    added input for searchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 function searchBar() {
-
+    return (
+        <div className="SearchBar">
+          <input name='searchBar' type='text' placeholder="Enter A Song, Album, or Artist" />
+        </div>
+      );
 }
 
 export default searchBar;
