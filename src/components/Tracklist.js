33d8de3b39e5e75f54a86a3aefe09e6a37b54b54
EWE Code <111604338+eatwanderexplore@users.noreply.github.com>
2024-02-27 13:16:07 +1300

    added tracklist return display

--- src/components/Tracklist.js
+++ src/components/Tracklist.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function tracklist() {
-
+    return (
+        <div className="TrackList">
+          {tracks.map(track => <Track key={track.id} track={track} />)}
+        </div>
+      );
 }
 
 export default tracklist;
