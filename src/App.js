6a7d584127409c93baae9d08a78c6be54aaf2f2d
EWE Code <111604338+eatwanderexplore@users.noreply.github.com>
2024-03-01 13:09:34 +1300

    added functions to update playlist

--- src/App.js
+++ src/App.js
@@ -5,24 +5,51 @@ import SearchResults from './components/SearchResults';
 import Playlist from './components/Playlist';
 
 function App() {
+  // Define state to store information
   const [searchResults, setSearchResults] = useState([
     { id: '1', name: 'Song 1', artist: 'Artist 1', album: 'Album 1' }
   ]);
+  
+  const [playlistName, setPlaylistName] = useState('My Playlist');
 
   const [playlistTracks, setPlaylistTracks] = useState([
     { id: '2', name: 'Song 2', artist: 'Artist 2', album: 'Album 2' }
   ]);
 
+  // Function to handle changes in playlist name
+  const handlePlaylistNameChange = (event) => {
+    setPlaylistName(event.target.value);
+  };
+
+  // Function to handle adding tracks to the playlist
+  const addTrackToPlaylist = (track) => {
+    setPlaylistTracks([...playlistTracks, track]);
+  };
+
+  // Function to handle removing tracks from the playlist
+  const removeTrackFromPlaylist = (trackId) => {
+    setPlaylistTracks(playlistTracks.filter(track => track.id !== trackId));
+  };
+
   return (
     <div className="App">
       <h1>Jammming</h1>
-      <SearchBar />
+      <input
+        type="text"
+        value={playlistName}
+        onChange={handlePlaylistNameChange}
+      />
       <div className="App-playlist">
-        <SearchResults searchResults={searchResults} />
-        <Playlist playlistTracks={playlistTracks} />
+        <Playlist
+          playlistName={playlistName}
+          playlistTracks={playlistTracks}
+        />
+        <TrackList
+          tracks={playlistTracks}
+          onAddTrack={addTrackToPlaylist}
+          onRemoveTrack={removeTrackFromPlaylist}
+        />
       </div>
-      <button className="SaveToSpotifyButton">Save To Spotify</button>
-      <button className="SearchButton">Search</button>
     </div>
   );
 }
